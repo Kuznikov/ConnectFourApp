0ec486599e3e0da1d49eac79f25cd958681638fd
Kuznikov <maksimkuznikov@mail.ru>
2023-04-23 21:07:05 +0300

    Clean Project
    A clean project in android studio to start implementing the application.
    *No tool bar
    *Fullsreen
    *LAYOUT_IN_DISPLAY_CUTOUT_MODE_SHORT_EDGES

--- ConnectFour/app/src/main/java/com/example/connectfour/MainActivity.kt
+++ ConnectFour/app/src/main/java/com/example/connectfour/MainActivity.kt
@@ -2,10 +2,14 @@ package com.example.connectfour
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.view.Window
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        val w: Window = window
+        w.decorView.systemUiVisibility = View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
         setContentView(R.layout.activity_main)
     }
 }
