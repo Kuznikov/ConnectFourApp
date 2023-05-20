8b26ca1ee6c0066a0b6cc7f3f6302f8b52693b4c
Kuznikov <maksimkuznikov@mail.ru>
2023-05-20 23:19:06 +0300

    fix: changing the game logic, reverting to a previous version

--- ConnectFour/app/src/main/java/com/example/connectfour/views/OnePlayerFragment.kt
+++ ConnectFour/app/src/main/java/com/example/connectfour/views/OnePlayerFragment.kt
@@ -6,40 +6,26 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import com.example.connectfour.databinding.FragmentOnePlayerBinding
-import com.example.connectfour.models.BoardModel
-import com.example.connectfour.viewmodels.BoardViewModel
 
-class OnePlayerFragment : Fragment(), BoardModel.OnBoardChangeListener {
+
+
+class OnePlayerFragment : Fragment() {
     private var _binding: FragmentOnePlayerBinding? = null
     private val mBinding get() = _binding!!
-    private lateinit var boardAdapter: BoardAdapter
-    private lateinit var viewModel: BoardViewModel
 
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
-        _binding = FragmentOnePlayerBinding.inflate(inflater, container, false)
+        _binding = FragmentOnePlayerBinding.inflate(layoutInflater, container, false)
         val gridView = mBinding.boardGridView
-
-        val boardModel = BoardModel(requireContext(), this)
-        viewModel = BoardViewModel(requireContext(), boardModel)
-
-        boardAdapter = BoardAdapter(requireContext(), viewModel)
-        gridView.adapter = boardAdapter
+        gridView.adapter = BoardAdapter(requireContext())
         gridView.numColumns = 7
+        gridView.adapter = BoardAdapter(requireContext())
 
         return mBinding.root
     }
-
-    override fun onDestroyView() {
-        super.onDestroyView()
-        _binding = null
-    }
-
-    override fun onBoardChanged() {
-        boardAdapter.notifyDataSetChanged()
-    }
 }
+
 
