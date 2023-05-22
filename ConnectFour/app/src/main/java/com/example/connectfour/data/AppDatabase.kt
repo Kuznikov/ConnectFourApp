f5100f54901c8ef78c25050c757df15da1d58c11
Kuznikov <maksimkuznikov@mail.ru>
2023-05-22 18:40:56 +0300

    fix: project cleaning

--- ConnectFour/app/src/main/java/com/example/connectfour/data/AppDatabase.kt
+++ ConnectFour/app/src/main/java/com/example/connectfour/data/AppDatabase.kt
@@ -4,17 +4,11 @@ import android.content.Context
 import androidx.room.Database
 import androidx.room.Room
 import androidx.room.RoomDatabase
-import com.example.connectfour.models.Game
-import com.example.connectfour.models.Move
-import com.example.connectfour.models.Player
 import com.example.connectfour.models.PlayerStats
 import com.example.connectfour.models.User
 
-@Database(entities = [Game::class, Player::class, Move::class, User::class, PlayerStats::class], version = 2)
+@Database(entities = [User::class, PlayerStats::class], version = 2)
 abstract class AppDatabase : RoomDatabase() {
-    abstract fun gameDao(): GameDao
-    abstract fun playerDao(): PlayerDao
-    abstract fun moveDao(): MoveDao
     abstract fun userDao(): UserDao
     abstract fun playerStatsDao(): PlayerStatsDao
 
